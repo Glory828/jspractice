3a0a1678af9323eda0569416e4232dfde1b705ec
Glory828 <duttariya288@gmail.com>
2025-04-26 13:20:49 +0000

    v4

---
+++ basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict";
+// alert("Welcome")  //we are using node.js not browser
+
+console.log(22+5);
+
+let name = "Jack Doe";
+let age="24";
+let isLoggedIn=true;
+let user=null
+//number 2 to power 53
+//string=""
+//boolean =>true/false
+//undefined
+//null => no value
+//  symbol=>unique
+
+//object
+console.log(typeof undefined); //string
+console.log(typeof null); //string
+
+//
--- basics/variable.js
+++ basics/variable.js
@@ -2,3 +2,11 @@ const accountId=14455889
 let accountEmail="Riya34@gmail.com"
 var accountPassword="45@80"
 accountCity="Shyamnagar"
+let accountState
+
+// accountId=2  //not allowed
+accountEmail="twak.com"
+accountPassword="545445"
+accountCity="kolkata"
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
